079673084e68a6619fe6caf0e5dc766fb1e6cbe7
pthegner <pierre.thegner@canaltp.fr>
2020-03-10 13:58:18 +0100

    Update types.py

--- flask_restful/types.py
+++ flask_restful/types.py
@@ -50,7 +50,12 @@ def natural(value):
 
 
 def boolean(value):
-    """Parse the string "true" or "false" as a boolean (case insensitive)"""
+    """Parse the string ``"true"`` or ``"false"`` as a boolean (case
+    insensitive). Also accepts ``"1"`` and ``"0"`` as ``True``/``False``
+    (respectively). If the input is from the request JSON body, the type is
+    already a native python boolean, and will be passed through without
+    further parsing.
+    """
     if isinstance(value, bool):
         return value
 
